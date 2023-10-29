0a812a5eeb4a162d39e3c86b4c0d661209f6d447
rahulsharma <rsharma0374@gmail.com>
2023-10-29 14:35:34 +0530

    Added jwt security

--- src/main/java/com/chat/userAuthentication/userController/ApiController.java
+++ src/main/java/com/chat/userAuthentication/userController/ApiController.java
@@ -10,16 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 
 @RestController
-@RequestMapping
+@RequestMapping("/user")
 public class ApiController {
 
     private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
@@ -52,5 +49,11 @@ public class ApiController {
         }
         return null;
 
+    }
+
+    //
+    @GetMapping("/hello")
+    public String getResult(){
+        return "hello";
     }
 }
